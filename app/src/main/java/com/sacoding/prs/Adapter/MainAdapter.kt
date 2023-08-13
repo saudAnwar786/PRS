023d4305938db191d0bc072f4909bf3c830f571e
Ayan Chaudhary <ayanchaudhary14@gmail.com>
2023-08-13 14:18:21 +0530

    added till adapter

---
+++ app/src/main/java/com/sacoding/prs/Adapter/MainAdapter.kt
@@ -0,0 +1,24 @@
+package com.sacoding.prs.Adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.sacoding.prs.databinding.Fragment1Binding
+
+class MainAdapter : RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
+
+    inner class MainViewHolder(val binding: Fragment1Binding):RecyclerView.ViewHolder(binding.root)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
