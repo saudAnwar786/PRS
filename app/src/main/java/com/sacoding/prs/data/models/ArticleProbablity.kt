1548cac1bd4c9bc354ac2562e94fd84320ae0adf
Ayan Chaudhary <ayanchaudhary14@gmail.com>
2023-08-19 13:00:36 +0530

    1

---
+++ app/src/main/java/com/sacoding/prs/data/models/ArticleProbablity.kt
@@ -0,0 +1,8 @@
+package com.sacoding.prs.data.models
+
+data class ArticleProbablity(
+    val article_id: Double,
+    val probability: Double,
+    val prod_name: String,
+    val product_code: Double
+)
