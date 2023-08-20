b19da2de3f12acdda0e95bdf092cbc1a50d5944c
Ayan Chaudhary <ayanchaudhary14@gmail.com>
2023-08-21 05:06:56 +0530

    final commit

--- app/src/main/java/com/sacoding/prs/ui/fragments/Fragment2.kt
+++ app/src/main/java/com/sacoding/prs/ui/fragments/Fragment2.kt
@@ -1,22 +1,20 @@
 package com.sacoding.prs.ui.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.sacoding.prs.Adapter.SupportAdapter
 import com.sacoding.prs.R
 import com.sacoding.prs.data.models.SupportItem
-import com.sacoding.prs.databinding.Fragment2Binding
+import com.sacoding.prs.databinding.FragmentSupportBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class Fragment2 :Fragment(R.layout.fragment2){
-    private lateinit var binding: Fragment2Binding
+class Fragment2 :Fragment(R.layout.fragment_support){
+    private lateinit var binding: FragmentSupportBinding
     private lateinit var supportAdapter : SupportAdapter
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding = Fragment2Binding.bind(view)
+        binding = FragmentSupportBinding.bind(view)
         super.onViewCreated(view, savedInstanceState)
 
         setUpRecyclerView()
